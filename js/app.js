8569eaf80f5cce41ee85fb00e95ee66f52e7c152
dimsand <d.sandron@it-akademy.fr>
2017-08-28 15:27:38 +0200

    Add ngRoute

--- js/app.js
+++ js/app.js
@@ -1,9 +1,22 @@
-var app = angular.module('MoodApp', []);
+var app = angular.module('MoodApp', ['ngRoute']);
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/home.html',
+      controller : 'parentCtrl'
+    }).
+    when('/search/', {
+      templateUrl: 'partials/search.html'
+    }).
+    when('/home/', {
+      templateUrl: 'partials/home.html'
+    });
+});
 
 app.controller('parentCtrl', function parentCtrl($scope, $http) {
-  $scope.title = 'Hello';
+  $scope.title = 'wtf';
   $http.get('http://api.moodify.dev/api/home/lyon').then(function(response) {
-    $scope.services = response.data;
+    $scope.services = response.data.returns;
     // console.log(response.data);
   });
 });
