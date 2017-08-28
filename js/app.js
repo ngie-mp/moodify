8c7dda66eaf5522a43cff4117203a8d92b9ba4f5
dimsand <d.sandron@it-akademy.fr>
2017-08-28 16:34:41 +0200

    Add googleAuth login function

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-var app = angular.module('MoodApp', ['ngRoute']);
+var app = angular.module('MoodApp', ['ngRoute', 'socialLogin']);
 app.config(function($routeProvider) {
   $routeProvider
     .when('/', {
-      templateUrl: 'partials/home.html',
-      controller : 'parentCtrl'
+      templateUrl: 'partials/login.html',
+      controller : 'loginCtrl'
     }).
     when('/search/', {
       templateUrl: 'partials/search.html'
@@ -13,10 +13,34 @@ app.config(function($routeProvider) {
     });
 });
 
+app.config(function(socialProvider){
+  socialProvider.setGoogleKey("724346200475-sj3iure20vb2mse5m6ogjtsg9kb5qma2.apps.googleusercontent.com");
+  socialProvider.setLinkedInKey("YOUR LINKEDIN CLIENT ID");
+  socialProvider.setFbKey({appId: "YOUR FACEBOOK APP ID", apiVersion: "API VERSION"});
+});
+
 app.controller('parentCtrl', function parentCtrl($scope, $http) {
   $scope.title = 'wtf';
   $http.get('http://api.moodify.dev/api/home/lyon').then(function(response) {
     $scope.services = response.data.returns;
     // console.log(response.data);
   });
+});
+
+app.controller('loginCtrl', function($scope, socialLoginService) {
+  $scope.signout = function(){
+    socialLoginService.logout();
+    console.log($scope.userGoogle);
+	}
+	$scope.$on('event:social-sign-in-success', (event, userDetails)=> {
+		$scope.userGoogle = userDetails;
+		$scope.$apply();
+    if(userDetails.token != null){
+      window.location = '/#!/search/';
+    }
+	})
+	$scope.$on('event:social-sign-out-success', function(event, userDetails){
+		$scope.userGoogle = userDetails;
+	})
+
 });
---
+++ js/tools.js
@@ -0,0 +1,24 @@
+$(function () {
+    $(".btn").click(function () {
+        $(".form-signin").toggleClass("form-signin-left");
+        $(".form-signup").toggleClass("form-signup-left");
+        $(".frame").toggleClass("frame-long");
+        $(".signup-inactive").toggleClass("signup-active");
+        $(".signin-active").toggleClass("signin-inactive");
+        $(this).removeClass("idle").addClass("active");
+    });
+});
+
+$(function () {
+    $(".btn-signup").click(function () {
+        $(".nav").toggleClass("nav-up");
+        $(".form-signup-left").toggleClass("form-signup-down");
+        $(".frame").toggleClass("frame-short");
+    });
+});
+
+$(function () {
+    $(".btn-signin").click(function () {
+        $("#form_connexion").submit();
+    });
+});
